12879be22ab5616548aba9f12a158e3b5fe5c4c6
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-01-29 15:35:39 -0200

    Update wp-script-manager.php

--- wp-script-manager.php
+++ wp-script-manager.php
@@ -2,12 +2,12 @@
 /**
  * WP Script Manager
  * Enqueue and concatenates javascript and css files.
- * @version 1.0.0
+ * @version 2.0.0
  */
 
 $upload_dir = wp_upload_dir();
 
-define( 'WPSMAN_VERSION', '1.0.0' );
+define( 'WPSMAN_VERSION', '2.0.0' );
 define( 'WPSMAN_SCRIPTS_DIR', $upload_dir['basedir'] . '/wpsman' );
 define( 'WPSMAN_SCRIPTS_URL', $upload_dir['baseurl'] . '/wpsman' );
 
@@ -16,7 +16,7 @@ if ( !wp_is_writable( $upload_dir['basedir'] ) ) {
 	add_action( 'admin_notices', 'wpsman_error_notice' );
 	function wpsman_error_notice (){
 		$class = 'notice notice-error';
-		$message = __( 'Your <code>uploads</code> directory is <strong>not writable</strong>. This is necessary for <strong>WP Script Manager</strong> to work.', 'wpsman' );
+		$message = __( 'Your <code>wp-content/uploads</code> directory is <strong>not writable</strong>. <em>WP Script Manager</em> needs to create files in this directory to work.', 'wpsman' );
 		printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message );
 	}
 } else {
