aa02943803ae91d61f1199ca646093cd5aa1e5da
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-01-30 19:38:30 -0200

    new feature: `wpsman_build_priority` filter

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,6 +1,8 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'wpsman_build_javascript_and_css' );
+$wpsman_build_priority = apply_filters( 'wpsman_build_priority', 10 );
+
+add_action( 'wp_enqueue_scripts', 'wpsman_build_javascript_and_css', $wpsman_build_priority );
 function wpsman_build_javascript_and_css () {
 	// build javascript
 	do_action( 'wpsman_add_javascript_files' );
